63603d8c40bb7f70aeb7fe2df30aa5842d6a5df6
wjg <2315797259@qq.com>
2018-11-25 17:19:37 +0800

    :white_check_mark: Adding tests.

--- boomvc-mvc/src/main/java/me/stevenkin/boomvc/mvc/view/View.java
+++ boomvc-mvc/src/main/java/me/stevenkin/boomvc/mvc/view/View.java
@@ -1,4 +1,10 @@
 package me.stevenkin.boomvc.mvc.view;
 
+import me.stevenkin.boomvc.http.HttpRequest;
+import me.stevenkin.boomvc.http.HttpResponse;
+
+import java.util.Map;
+
 public interface View {
+    void render(Map<String, Object> model, HttpRequest request, HttpResponse response);
 }
---
+++ boomvc-mvc/src/test/java/me/stevenkin/boomvc/mvc/resolver/ParameterResolverTest.java
@@ -0,0 +1,43 @@
+package me.stevenkin.boomvc.mvc.resolver;
+
+import me.stevenkin.boomvc.http.HttpQueryParameter;
+import me.stevenkin.boomvc.http.HttpRequest;
+import me.stevenkin.boomvc.http.HttpResponse;
+import me.stevenkin.boomvc.mvc.annotation.QueryParam;
+import me.stevenkin.boomvc.mvc.resolver.imp.QueryParamResolver;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+public class ParameterResolverTest {
+
+    @Test
+    public void queryParamResolverTest() throws Exception {
+        MethodParameter methodParameter = mock(MethodParameter.class);
+        Method method = ParameterResolverTest.class.getDeclaredMethod("queryParam", new Class[]{String.class});
+        Annotation[][] annotations = method.getParameterAnnotations();
+        Type[] paramTypes = method.getGenericParameterTypes();
+        Annotation queryParam = annotations[0][0];
+        Type paramType = paramTypes[0];
+        when(methodParameter.getParameterAnnotation()).thenReturn(queryParam);
+        when(methodParameter.getParameterType()).thenReturn(paramType);
+
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.firstParameter("hello")).thenReturn(Optional.of(new HttpQueryParameter("hello", "boomvc")));
+
+        HttpResponse response = mock(HttpResponse.class);
+        ParameterResolver queryParamResolver = new QueryParamResolver();
+        Assert.assertTrue(queryParamResolver.support(methodParameter));
+        System.out.println(queryParamResolver.resolve(methodParameter, request, response));
+    }
+
+    public void queryParam(@QueryParam("hello") String hello){
+
+    }
+}
